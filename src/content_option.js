4e58a4dd26d30edf63ab4b01ccaa8b57af0faf24
aboubacar <diarraabba123@gmail.com>
2024-04-29 13:29:59 +0000

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+const logotext = "Mr Diarra";
+const meta = {
+    title: "Aboubacar Diarra",
+    description: "I’m Aboubacar Diarra Front-End developer,currently working in Mali",
+};
+
+const introdata = {
+    title: "I’m Aboubacar Diarra",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "Actually working in Database Management",
+    },
+    description: "Passionate and dedicated web developer in designing web-based applications.",
+    your_img_url: "https://i.im.ge/2024/04/23/ZL2tjc.image1.jpeg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Proficient in a variety of programming languages including HTML, CSS, JavaScript, and frameworks such as React, or Vue.js. Skilled in both front-end and in progress with back-end learning, with a strong understanding of responsive design principles and cross-browser compatibility.",
+};
+
+const skills = [{
+        name: "Python",
+        value: 50,
+    },
+    {
+        name: "Django",
+        value: 40,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Innovative and detail-oriented UI/UX designer with a passion for creating intuitive and visually appealing digital experiences. ",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/photo-1573867639040-6dd25fa5f597?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHdlYnNpdGV8ZW58MHx8MHx8fDA%3D",
+        description: "Skilled in translating design mockups into responsive and pixel-perfect web layouts.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/1/work-stations-plus-espresso.jpg?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Experienced web developer specializing in front-end development with a focus on creating engaging user interfaces and seamless user experiences..",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/7/367/267",
+        description: "Effective collaborator with a track record of working closely with designers and backend developers to bring projects to fruition.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Never count your age, passing years don’t matter for a happy face.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2Vic2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+        description: " Dedicated to staying updated with the latest web technologies and best practices.",
+        link: "#",
+    },
+
+    {
+        img: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vic2l0ZXxlbnwwfHwwfHx8MA%3D%3D",
+        description: " Proficient in HTML, CSS, and JavaScript, with expertise in modern frameworks such as React. ",
+        link: "#",
+    },
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "diarraaboubacar603@gmail.com",
+    YOUR_FONE: " +223 70285159",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_cok084q",
+    YOUR_TEMPLATE_ID: "template_43p9838",
+    YOUR_USER_ID: "hqfhRy6U1-SL5oCHo",
+};
+
+const socialprofils = {
+    github: "https://github.com/CodeWithAboubacar",
+    linkedin: "https://www.linkedin.com/in/aboubacar-diarra-99b583236",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
